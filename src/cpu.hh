8f63269a2205d5c9910057bfb4119ea38046ba86
Rayane AMROUCHE <rayane.amrouche@epita.fr>
2020-10-06 17:48:53 +0200

    [CPU] class CPU created

---
+++ src/cpu.hh
@@ -0,0 +1,71 @@
+#pragma once
+#include "xtensor/xarray.hpp"
+#include "xtensor/xio.hpp"
+#include "xtensor/xview.hpp"
+
+namespace CPU
+{
+    class ICP
+    {
+    public:
+        ICP(double s_, xt::xarray<double> t_, xt::xarray<double> r_,
+            xt::xarray<double> m_, xt::xarray<double> p_)
+            : s{s_},
+              t{t_},
+              r{r_},
+              m{m_},
+              p{p_},
+              new_p{p_},
+              np{p_.shape(1)},
+              nm{m_.shape(1)},
+              dim{p_.dimension()}
+        {
+        }
+
+        ~ICP();
+
+        void find_alignment();
+        double ICP::find_alignment(xt::xarray<double> y);
+
+        //Getter & Setter
+        inline double getS() { return s; }
+        inline void setS(double s_) { s = s_; }
+
+        inline xt::xarray<double> getR() { return r; }
+        inline void setR(xt::xarray<double> r_) { r = r_; }
+
+        inline xt::xarray<double> getT() { return t; }
+        inline void setT(xt::xarray<double> t_) { t = t_; }
+
+        inline xt::xarray<double> getP() { return p; }
+        inline void setP(xt::xarray<double> p_) { p = p_; }
+
+        inline xt::xarray<double> getM() { return t; }
+        inline void setM(xt::xarray<double> m_) { m = m_; }
+
+        inline xt::xarray<double> getNewP() { return new_p; }
+        inline void setNewP(xt::xarray<double> new_p_) { new_p = new_p_; }
+
+        inline int getMaxIter() { return max_iter; }
+        inline double getThreshold() { return threshold; }
+        inline unsigned int getNP() { return np; }
+        inline unsigned int getNM() { return nm; }
+        inline unsigned int getDim() { return dim; }
+
+    private:
+        double s;
+        xt::xarray<double> t;
+        xt::xarray<double> r;
+
+        xt::xarray<double> m;
+        xt::xarray<double> p;
+        xt::xarray<double> new_p;
+
+        unsigned int np;
+        unsigned int nm;
+        unsigned int dim;
+
+        constexpr int max_iter = 200;
+        constexpr double threshold = 1e-5;
+    };
+} // namespace CPU
