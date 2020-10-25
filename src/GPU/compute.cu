476d536696cf03718a3a4c46b547ae69cb9ee541
Yassir RAMDANI <yassir.ramdani@epita.fr>
2020-10-25 02:22:37 +0100

    wip

--- src/GPU/compute.cu
+++ src/GPU/compute.cu
@@ -92,7 +92,7 @@ __global__ void find_Y(double *distance, size_t distance_p,
 
     int minIdx = 0;
     for (int i = 1; i < xSize; i++) {
-        printf("> (%i, j) : current=%lf vs %lf \n",
+        printf("> (%i, %d) : current=%lf vs %lf \n",
                i, j, distance[minIdx + j * distance_p], distance[i + j+distance_p]);
         if (distance[minIdx + j*distance_p] > distance[i + j+distance_p]){
             minIdx = i;
