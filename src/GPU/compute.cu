4907af6fab477a31678576b5b6985dc3d8c7d0ec
Yassir RAMDANI <yassir.ramdani@epita.fr>
2020-10-25 02:57:44 +0100

    wip

--- src/GPU/compute.cu
+++ src/GPU/compute.cu
@@ -79,9 +79,12 @@ __global__ void compute_distance(double *m, size_t m_p, double *p, size_t p_p,
            i, j, distance[i + j * distance_p], p[j], p[j + p_p], p[j + 2*p_p], mx, my, mz);
 }
 
+
+
 __global__ void find_Y(double *distance, size_t distance_p,
                                   double *m, size_t m_p, double *Y, size_t Y_p,
                                   int xSize, int ySize) {
+
     int j = blockIdx.y*blockDim.y + threadIdx.y;
 
     if (j >= ySize)
@@ -93,8 +96,8 @@ __global__ void find_Y(double *distance, size_t distance_p,
     int minIdx = 0;
     for (int i = 1; i < xSize; i++) {
         printf("> (%i, %d) : current=%lf vs %lf \n",
-               i, j, distance[minIdx + j * distance_p], distance[i + j+distance_p]);
-        if (distance[minIdx + j*distance_p] > distance[i + j+distance_p]){
+               i, j, distance[minIdx + j * distance_p], distance[i + j*distance_p]);
+        if (distance[minIdx + j*distance_p] > distance[i + j*distance_p]){
             minIdx = i;
             printf("YES\n");
         }
