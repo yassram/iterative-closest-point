d85b44d618813675ec8be4bccb2d3efd7349a8ee
Rayane AMROUCHE <rayane.amrouche@epita.fr>
2020-10-27 18:00:56 +0100

    form 10 line to 3

--- src/GPU/gpu.cc
+++ src/GPU/gpu.cc
@@ -61,35 +61,44 @@ namespace GPU
         }
 
         Matrix mu_p{this->new_p.rowwise().mean()};
-        Matrix mu_y{y.rowwise().mean()};
+        Matrix mu_y{y.rowwise().mean().transposeInPlace()};
 
         Matrix p_prime = substract_col_w(this->new_p, mu_p);
         Matrix y_prime = substract_col_w(y, mu_y);
 
-        auto px = p_prime.row(0);
-        auto py = p_prime.row(1);
-        auto pz = p_prime.row(2);
+        Matrix s = p_prime * y_prime; //multiplication matricielle
 
-        auto yx = y_prime.row(0);
-        auto yy = y_prime.row(1);
-        auto yz = y_prime.row(2);
+        MatrixXd n_matrix{4, 4};
 
-        auto sxx = (px.array() * yx.array()).sum();
-        auto sxy = (px.array() * yy.array()).sum();
-        auto sxz = (px.array() * yz.array()).sum();
-        auto syx = (py.array() * yx.array()).sum();
-        auto syy = (py.array() * yy.array()).sum();
-        auto syz = (py.array() * yz.array()).sum();
-        auto szx = (pz.array() * yx.array()).sum();
-        auto szy = (pz.array() * yy.array()).sum();
-        auto szz = (pz.array() * yz.array()).sum();
+        n_matrix << s[0,0] + s[1,1] + s[2,2], s[1,2] - s[2,1], -1 * s[0,2] + s[2,0], s[0,1] - s[1,0],
+            -1 * s[2,1] + s[1,2], s[0,0] - s[2,2] - s[1,1], s[0,1] + s[1,0], s[0,2] + s[2,0],
+            s[2,0] - s[0, 2], s[1,0] + s[0,1], s[1,1] - s[2,2] - s[0,0], s[1,2] + s[2,1],
+            -1 * s[1,0] + s[0,1], s[2,0] + s[0, 2], s[2,1] + s[1,2], s[2,2] - s[1,1] - s[0,0];
 
-        MatrixXd n_matrix {MatrixXd{4, 4}};
+        // auto px = p_prime.row(0);
+        // auto py = p_prime.row(1);
+        // auto pz = p_prime.row(2);
 
-        n_matrix << sxx + syy + szz, syz - szy, -1 * sxz + szx, sxy - syx,
-            -1 * szy + syz, sxx - szz - syy, sxy + syx, sxz + szx,
-            szx - sxz, syx + sxy, syy - szz - sxx, syz + szy,
-            -1 * syx + sxy, szx + sxz, szy + syz, szz - syy - sxx;
+        // auto yx = y_prime.row(0);
+        // auto yy = y_prime.row(1);
+        // auto yz = y_prime.row(2);
+
+        // auto sxx = (px.array() * yx.array()).sum();
+        // auto sxy = (px.array() * yy.array()).sum();
+        // auto sxz = (px.array() * yz.array()).sum();
+        // auto syx = (py.array() * yx.array()).sum();
+        // auto syy = (py.array() * yy.array()).sum();
+        // auto syz = (py.array() * yz.array()).sum();
+        // auto szx = (pz.array() * yx.array()).sum();
+        // auto szy = (pz.array() * yy.array()).sum();
+        // auto szz = (pz.array() * yz.array()).sum();
+
+        // MatrixXd n_matrix {MatrixXd{4, 4}};
+
+        // n_matrix << sxx + syy + szz, syz - szy, -1 * sxz + szx, sxy - syx,
+        //     -1 * szy + syz, sxx - szz - syy, sxy + syx, sxz + szx,
+        //     szx - sxz, syx + sxy, syy - szz - sxx, syz + szy,
+        //     -1 * syx + sxy, szx + sxz, szy + syz, szz - syy - sxx;
 
 
         Eigen::EigenSolver<MatrixXd> eigen_solver(n_matrix);
