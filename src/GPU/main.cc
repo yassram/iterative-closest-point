2fa48cd87433ac2dae5928956b156cae129ad84f
Rayane AMROUCHE <rayane.amrouche@epita.fr>
2020-10-27 18:47:48 +0100

    test done

--- src/GPU/main.cc
+++ src/GPU/main.cc
@@ -28,10 +28,10 @@ int main(int argc, char* argv[])
     auto matrix_ref = load_matrix(argv[1], min_coord_ref, max_coord_ref);
     auto matrix_scene = load_matrix(argv[2], min_coord_scene, max_coord_scene);
 
-    // GPU::ICP icp(matrix_ref, matrix_scene, max_iter);
+    GPU::ICP icp(matrix_ref, matrix_scene, max_iter);
 
-    // icp.find_corresponding();
-    // write_matrix(icp.new_p);
+    icp.find_corresponding();
+    write_matrix(icp.new_p);
 
     return 0;
 }
