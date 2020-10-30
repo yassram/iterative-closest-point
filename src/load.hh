ca08d2c1d6c3396b59bdb48e2f3f6e401318c5c8
Yassir RAMDANI <yassir.ramdani@epita.fr>
2020-10-31 00:59:40 +0100

    Revert "Tests with tr at load"
    This reverts commit 5672c1b8f4faaaed36ec4d49c2b10a5cf6aa60a8.

--- src/load.hh
+++ src/load.hh
@@ -6,6 +6,6 @@
 #include "GPU/gpu.hh"
 #include "GPU/compute.hh"
 
-GPU::Matrix load_matrix(const char *filename, GPU::Matrix& min_coord,
-                        GPU::Matrix& max_coord);
+GPU::Matrix load_matrix(const char *filename, GPU::Matrix min_coord,
+                        GPU::Matrix max_coord);
 void write_matrix(GPU::Matrix matrix);
