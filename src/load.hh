d0a044e2fb700359b7d2ae1fdf954dcf3b10d9f0
Amine Heffar <amine.heffar@r01p03.sm14.sm.cri.epita.net>
2020-10-30 19:08:03 +0100

    [OPTI] batch_size, block and grid sizes optimised

--- src/load.hh
+++ src/load.hh
@@ -6,7 +6,7 @@
 #include "GPU/gpu.hh"
 #include "GPU/compute.hh"
 
-GPU::Matrix load_matrix(const char *filename, double min_coord[3], double max_coord[3]);
+GPU::Matrix load_matrix(const char *filename, GPU::Matrix min_coord, GPU::Matrix max_coor);
 void write_matrix(GPU::Matrix matrix);
 void write_matrix(Eigen::MatrixXd matrix);
 MatrixXd cpu_load_matrix(const char *filename, double min_coord[3], double max_coord[3]);
